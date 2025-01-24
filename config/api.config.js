cb149cb1a78b2d13ec5c85ab4741148b18fead9d
wuhunfeng <wuhunfeng@163.com>
2025-01-24 11:37:24 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -12,7 +12,8 @@ module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth.
   // If you are an E5 developer subscriber, set them in Vercel's environment variables `CLIENT_ID` and `CLIENT_SECRET`
   // You would not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-
+  clientId: '42c5f1eb-b4b1-420c-ba88-0f011b100200',
+  obfuscatedClientSecret: 'U2FsdGVkX19AaE7vbU0N1UDRbIBNH2flELjQJX0FkGo8z57OiZmHHtQipMWPQbkifyF7Pk+jmR6UUNWdN0JVLw==',
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
   redirectUri: 'http://localhost',
